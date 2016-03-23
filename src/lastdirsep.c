344c88fc26cfe0b045c7e08a9cb33ba360e0cccf
James Hunt <james@niftylogic.com>
2016-03-23 14:13:11 -0400

    indent / reformat code (linux-style)

--- src/lastdirsep.c
+++ src/lastdirsep.c
@@ -5,17 +5,19 @@
  *	#include "../README"						*
  ************************************************************************/
 #ifdef RCS
-static /*const*/char rcsid[]=
- "$Id: lastdirsep.c,v 1.4 1997/04/03 01:58:44 srb Exp $";
+static /*const */ char rcsid[] =
+    "$Id: lastdirsep.c,v 1.4 1997/04/03 01:58:44 srb Exp $";
 #endif
 #include "includes.h"
 #include "lastdirsep.h"
 
 extern const char dirsep[];
 
-char*lastdirsep(filename)const char*filename;	 /* finds the next character */
-{ const char*p;					/* following the last DIRSEP */
-  while(p=strpbrk(filename,dirsep))
-     filename=p+1;
-  return (char*)filename;
+char *lastdirsep(filename)
+const char *filename;		/* finds the next character */
+{
+	const char *p;		/* following the last DIRSEP */
+	while (p = strpbrk(filename, dirsep))
+		filename = p + 1;
+	return (char *)filename;
 }
